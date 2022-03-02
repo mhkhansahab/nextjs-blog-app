be28f5731254213401468146913315b60e3875ee
mhkhansahab <hamza.khansahab@gmail.com>
2022-03-02 10:47:32 +0500

    updated dynamic route

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,33 +1,42 @@
-import { NextPage, GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from "next";
+import { NextPage, GetStaticProps, GetStaticPaths, InferGetStaticPropsType, GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Image from "next/image";
 import styles from "../styles/blog.module.css";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-    const blogs = await fetch('https://nestjs-blog-app.herokuapp.com/blogs');
-    const jsonResponse = await blogs.json();
-    const paths = jsonResponse.map((blog: any) => {
-        return {
-            params: { id: blog?._id }
-        }
-    })
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     const blogs = await fetch('https://nestjs-blog-app.herokuapp.com/blogs');
+//     const jsonResponse = await blogs.json();
+//     const paths = jsonResponse.map((blog: any) => {
+//         return {
+//             params: { id: blog?._id }
+//         }
+//     })
 
-    return {
-        paths,
-        fallback: false
-    };
-}
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
 
-export const getStaticProps: GetStaticProps = async (context: any) => {
-    const id = context.params.id;
+// export const getStaticProps: GetStaticProps = async (context: any) => {
+//     const id = context.params.id;
+//     const response = await fetch('https://nestjs-blog-app.herokuapp.com/blogs/' + id);
+//     const blog: any = await response.json();
+//     return {
+//         props: { blog }
+//     }
+// }
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+    const { id } = context?.query;
     const response = await fetch('https://nestjs-blog-app.herokuapp.com/blogs/' + id);
     const blog: any = await response.json();
+
     return {
         props: { blog }
     }
 }
 
-
-const Blog: NextPage = ({ blog }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Blog: NextPage = ({ blog }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
         <div className={styles.container}>
             <div className={styles.title}>{blog?.title}</div>
